74c22ad53f14f6fc16fb80895da615eaf9f8702d
ElaineZhou-moon <elainemiomio@gmail.com>
2023-11-02 23:40:40 -0400

    a4 State and Redux fa23

--- src/Nav.js
+++ src/Nav.js
@@ -4,8 +4,9 @@ function Nav() {
     const { pathname } = useLocation();
     return (
         <nav className='nav nav-tabs mt-2'>
-            <Link to='/hello' className={`nav-link ${pathname.includes('hello') ? "active" : ""}`}>Hello</Link>
             <Link to='/Labs/a3' className={`nav-link ${pathname.includes('a3') ? 'active' : ''}`}>Assignment 3</Link>
+            <Link to='/Labs/a4' className={`nav-link ${pathname.includes('a4') ? 'active' : ''}`}>Assignment 4</Link>
+            <Link to='/hello' className={`nav-link ${pathname.includes('hello') ? "active" : ""}`}>Hello</Link>
             <Link to='/Kanbas' className={`nav-link ${pathname.includes('Kanbas') ? 'active' : ''}`}>Kanbas</Link>
         </nav>
     )
